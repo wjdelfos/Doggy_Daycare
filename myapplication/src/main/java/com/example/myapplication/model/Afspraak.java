c7cc5f59b4aab6fd0b23b505e46a141e4e92556e
wjdelfos <38474111+wjdelfos@users.noreply.github.com>
2020-10-10 14:35:01 +0200

    Variables added afspraak en review

--- myapplication/src/main/java/com/example/myapplication/model/Afspraak.java
+++ myapplication/src/main/java/com/example/myapplication/model/Afspraak.java
@@ -1,4 +1,10 @@
 package com.example.myapplication.model;
 
+import java.util.UUID;
+
 public class Afspraak {
+    private UUID ID;
+    private double AfgesprokenPrijs;
+    private enum Status{concept,geaccepteerd,afgewezen,betaald}
+
 }
