77406d5546790a558217bd9d2f88c6b18b78edf5
remco <remcostuij@gmail.com>
2020-10-18 15:35:33 +0200

    Favourite button klikken werkt weer.

--- myapplication/src/main/java/com/example/myapplication/ui/advertentie/AdvertentieRecyclerFragment.java
+++ myapplication/src/main/java/com/example/myapplication/ui/advertentie/AdvertentieRecyclerFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -86,6 +87,9 @@ public class AdvertentieRecyclerFragment extends Fragment {
         private TextView mDateTextView;
         private TextView mOwnerTextView;
         private TextView mLocationTextView;
+        private ImageButton mFavourite;
+        private Boolean favourite;
+
 
         public AdvertentieHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.row_advertentie, parent, false));
@@ -103,6 +107,7 @@ public class AdvertentieRecyclerFragment extends Fragment {
             mDateTextView = (TextView) itemView.findViewById(R.id.Date);
             mOwnerTextView = (TextView) itemView.findViewById(R.id.Owner);
             mLocationTextView = (TextView) itemView.findViewById(R.id.Location);
+
         }
 
         public void bind(Advertentie advertentie) {
@@ -111,6 +116,21 @@ public class AdvertentieRecyclerFragment extends Fragment {
             mDateTextView.setText(mAdvertentie.getBeginTijd().toString());
             mOwnerTextView.setText(mAdvertentie.get_AdvertentiePlaatser().getNaam());
             mLocationTextView.setText(mAdvertentie.getLocatie());
+            mFavourite = (ImageButton) itemView.findViewById(R.id.imageButton);
+            mFavourite.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    // wanneer favourite gedefinieerd is kan onderstaande zin weg en kunnen de comments weg
+                    // TODO favorite definieren
+                    mFavourite.setBackgroundResource(R.drawable.favourite_button_gekleurd);
+                    //  if (favourite == false) {
+                    //      mFavourite.setBackgroundResource(R.drawable.favourite_button_gekleurd);
+                    //      favourite = true;
+                    //  } else {
+                    //      mFavourite.setBackgroundResource(R.drawable.favourite_button);
+                    //  }
+
+                }
+            });
         }
     }
 
